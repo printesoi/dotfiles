c8847e2e9b92e0e28ed1abf0c623712953bd456b
Victor Dodon <dodonvictor@gmail.com>
2012-10-12 18:11:43 +0300

    Add git_files function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -2,3 +2,17 @@ function mkdircd()
 {
     mkdir -p "$@" && eval cd "\"\$$#\"";
 }
+
+function git_files()
+{
+    git status &> /dev/null || return 1
+
+    case "$1" in
+        "m"|"M") regex=" M";;
+        "d"|"D") regex=" D";;
+        "a"|"A") regex="A ";;
+        "u"|"U") regex="\?\?";;
+        *) return 2;;
+    esac
+    git status --porcelain | sed -nre "s/^$regex (.*)$/\1/p"
+}
