66ac049bb1f3a8af54c78683172bc10ffac28ccb
Victor Dodon <dodonvictor@gmail.com>
2017-08-17 13:31:31 +0300

    Allow disabling vim and nvim config

--- setup.sh
+++ setup.sh
@@ -3,30 +3,43 @@
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${HOME}/.zprezto"
 git clone --recursive https://github.com/printesoi/dotfiles.git "${HOME}/dotfiles"
 
-for rcfile in "${HOME}/dotfiles/"{{,g}vimrc,zshrc,zshenv,zpreztorc,gitconfig,lesskey}; do
-    ln -s "$rcfile" "${HOME}/.${rcfile:t}"
+for rcfile in "${HOME}/dotfiles/"{zshrc,zshenv,zpreztorc,gitconfig,lesskey}; do
+    ln -s "${rcfile}" "${HOME}/.${rcfile:t}"
 done
 
 lesskey
 
-
 # Vim
-ln -s "${HOME}/dotfiles/vimfiles" "${HOME}/.vim"
-mkdir -p "${HOME}/.vim/tmp"
+if [[ -z "${PRINTESOI_DOTFILES_DISABLE_VIM}" ]]; then
+    VIMDIR="${HOME}/.vim"
 
-git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.vim/bundle/Vundle.vim"
+    ln -s "${HOME}/dotfiles/vimfiles" "${VIMDIR}"
+    mkdir -p "${VIMDIR}/tmp"
 
-# This is still interactive
-vim +PluginInstall +qall
+    for rcfile in {,g}vimrc; do
+        ln -s "${HOME}/dotfiles/${rcfile}" "${HOME}/.${rcfile:t}"
+    done
+
+    git clone https://github.com/VundleVim/Vundle.vim.git "${VIMDIR}/bundle/Vundle.vim"
+    git clone https://github.com/printesoi/gruvbox.git "${VIMDIR}/bundle/gruvbox"
+
+    # This is still interactive
+    vim +PluginInstall +qall
+fi
 
 
 # Neovim
-mkdir -p "${HOME}/.config/nvim/bundle/"
-for rcfile in init.vim ginit.vim; do
-    ln -s "${HOME}/dotfiles/nvim/${rcfile}" "${HOME}/.config/nvim/${rcfile}"
-done
+if [[ -z "${PRINTESOI_DOTFILES_DISABLE_NVIM}" ]]; then
+    NVIMDIR="${HOME}/.config/nvim"
 
-git clone https://github.com/VundleVim/Vundle.vim.git "${HOME}/.config/nvim/bundle/Vundle.vim"
+    mkdir -p "${NVIMDIR}/bundle/"
+    for rcfile in init.vim ginit.vim; do
+        ln -s "${HOME}/dotfiles/nvim/${rcfile}" "${NVIMDIR}/${rcfile}"
+    done
 
-# This is still interactive
-nvim +PluginInstall +qall
+    git clone https://github.com/VundleVim/Vundle.vim.git "${NVIMDIR}/bundle/Vundle.vim"
+    git clone https://github.com/printesoi/gruvbox.git "${NVIMDIR}/bundle/gruvbox"
+
+    # This is still interactive
+    nvim +PluginInstall +qall
+fi
